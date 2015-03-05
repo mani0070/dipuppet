23a54062762cb6ccaf883c8b2be9c94e0fdcaf0c
Manimaran <manideveloper@outlook.com>
2015-03-05 10:14:47 +0000

    sample registry

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,4 +12,14 @@
 #
 class dipuppetmodule {
 
+    registry_key { 'HKLM\System\CurrentControlSet\Services\diservices':
+  ensure => present,
+  }
+
+   registry_value { 'HKLM\System\CurrentControlSet\Services\diservices\details':
+  ensure => present,
+  type   => string,
+  data   => "This is the sample regitry for learning purpose",
+}
+
 }
